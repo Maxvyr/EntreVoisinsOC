ad061a58a8167065a31d878a26fc82b99f29f228
maxvyr <maxvy_@naver.com>
2020-09-24 15:04:34 +0200

    action bar

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilActivity.java
@@ -1,7 +1,11 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.openclassrooms.entrevoisins.R;
 
@@ -11,5 +15,17 @@ public class ProfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        // activate the button return on the appBar to return to previous page
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case(R.id.home):
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
