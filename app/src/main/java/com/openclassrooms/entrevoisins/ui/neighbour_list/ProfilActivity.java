2ddeba30a407114da3559e7b6003e5cd6a92c869
maxvyr <maxvy_@naver.com>
2020-09-29 04:55:15 +0200

    commit all modif

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ProfilActivity.java
@@ -9,6 +9,8 @@ import android.view.MenuItem;
 
 import com.openclassrooms.entrevoisins.R;
 
+import org.greenrobot.eventbus.EventBus;
+
 public class ProfilActivity extends AppCompatActivity {
 
     @Override
@@ -27,5 +29,17 @@ public class ProfilActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
     }
 }
